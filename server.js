e39ba3f540b709ad50e8e4718b896b9c21824f19
ngonzano <ng.rifas.jl@gmail.com>
2023-12-15 22:35:08 -0500

    .

--- server.js
+++ server.js
@@ -89,8 +89,8 @@ efectivo(app)
 yapeController(app)
 culqiController(app)
 
-// server.listen(port,'0.0.0.0', function(){
-server.listen(port,'192.168.18.8'||'localhost', function(){
+server.listen(port,'0.0.0.0', function(){
+// server.listen(port,'192.168.18.8'||'localhost', function(){
     console.log('App '+process.pid+' iniciada...')
     console.log('Port '+port+' iniciada...')
 })
